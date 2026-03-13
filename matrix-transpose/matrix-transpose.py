cee3f83bcec8175d42292a163833c3f009e02705
Thanya Gowtham <86415860+thanya-gowtham@users.noreply.github.com>
2026-03-13 12:39:24 +0530

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -6,4 +6,12 @@ def matrix_transpose(A):
     """
     A = np.array(A)
     return np.transpose(A)
+
+    # r = len(A)
+    # c = len(A[0])
+    # res = [[0] * r for _ in range(c)]
+    # for i in range(r):
+    #     for j in range(c):
+    #         res[j][i] = A[i][j]
+    # return res
     
